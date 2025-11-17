4cea469e834a54ee3789fc055cc839fc7d631fdb
Skyy Moore <mooreapps2022@gmail.com>
2025-11-16 20:17:29 -0800

    I have added a pizza page to play around with HTML in PHP.

--- srcs/requirements/wordpress/index.php
+++ srcs/requirements/wordpress/index.php
@@ -5,3 +5,24 @@ echo "It's really good! <br>";
 echo "It's really, really good!";
 ?>
 
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Inception</title>
+	<link rel="stylesheet" href="styles.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<br>
+	<a href="/index.html">
+		<button>order pizza</button>
+	</a>
+</body>
+
+</html>
